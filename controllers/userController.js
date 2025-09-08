8ca29f2e03b42529a22f010f86e19ac842b47127
krish12345q89 <krish.arora@qualyval.com>
2025-09-08 18:01:09 +0530

    fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 // Multer setup
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, '/tmp');
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
